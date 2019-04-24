01115ea5e65131758618fafea9a178c49d4bb341
shadowpl4ys@gmail.com <shadowpl4ys@gmail.com>
2019-04-24 00:59:03 -0300

    re

---
+++ PoS Tagging/pos_tag_analyzer.py
@@ -0,0 +1,18 @@
+corpus = "((S (SBJ I) (NOUN uh) (NOT don't) (NEXP like) (OBJ it) (UNK re)))"
+corpus = corpus.replace("(", "").replace(")", "").replace("S", "").split()
+#separador de tags e palvras:
+tags = []
+words = []
+for index, word in enumerate(corpus):
+    if index % 2 == 0:
+        tags.append(word)
+    else:
+        words.append(word)
+
+unigrams = {}
+
+tag_counter = {}
+
+print(corpus)
+print(words)
+print(tags)
